2d035d421d238ffe5bdad04797c26cdba953a7a9
Aaronthetechguy <70182159+Aaronthetechguy@users.noreply.github.com>
2020-12-10 10:30:42 -0500

    update auton

--- BlueLeftNew.java
+++ BlueLeftNew.java
@@ -17,58 +17,54 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 
-public class BlueLeft extends OpMode{
+public class BlueLeftNew extends OpMode {
     double rev = 383.6;
-    double inch = 2*rev/(3.78*3.14);
-    double feet = inch*12;
-        private ElapsedTime runtime = new ElapsedTime();
+    double inch = 2 * rev / (3.78 * 3.14);
+    double feet = inch * 12;
+    private ElapsedTime runtime = new ElapsedTime();
     private DigitalChannel DigChannel;
     AutonMethods_Copy robot = new AutonMethods_Copy();
-    
+
     public void init() {
         robot.init(hardwareMap, telemetry, false);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        
+
         robot.changeRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
-    
+
     @Override
     public void start() {
         runtime.reset();
     }
-    
+
     @Override
     public void loop() {
-        
+
         switch (robot.counter) {
-        case 0:
-            robot.drive(4*feet, 5*feet, .6);
-            robot.counter++;
-            break;
-        case 1:
-            robot.shoot(1800, 0.7, 1);
-            robot.counter++;
-            break;
-        case 2:
-            robot.drive(-.5*feet, 0, .6);
-            robot.counter++;
-            break;
-        case 3:
-            robot.shoot(1800, 0, 1);
-            robot.counter++;
-            break;
-        case 4:
-            robot.drive(-.5*feet,0, .6);
-            robot.counter++;
-            break;
-        case 5:
-            robot.shoot(1800, 0, 1);
-            robot.counter++;
-            break;
+            case 0:
+                robot.drive(2 * feet, 2 * feet, 5);
+                robot.counter++;
+                break;
+            case 1:
+                robot.drive(2 * feet, 0, 5);
+                break;
+            case 2:
+                robot.shoot();
+                break;
+            case 3:
+                int rings = robot.distance();
+                if (rings == 4) {
+                    robot.drive(-1 * feet, 4 * feet, 5);
+                } else if (rings == 1) {
+                    robot.drive(o, 3 * feet, 5);
+                } else {
+                    robot.drive(-1 * feet, 1 * feet, 5);
+                }
+                break;
+
+
+                telemetry.addData("Case:", robot.counter);
+                telemetry.update();
+        }
     }
-        
-        telemetry.addData("Case:", robot.counter);
-        telemetry.update();
-    }
-}
