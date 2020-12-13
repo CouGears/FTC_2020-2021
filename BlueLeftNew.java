4b6f519a0582436559a223b700814fbb1a6cb5d7
Aaronthetechguy <70182159+Aaronthetechguy@users.noreply.github.com>
2020-12-13 14:22:51 -0500

    autonMethods

--- BlueLeftNew.java
+++ BlueLeftNew.java
@@ -43,34 +43,51 @@ public class BlueLeftNew extends OpMode {
 int rings = 0;
         switch (robot.counter) {
             case 0:
-                robot.drive(2 * feet, 2 * feet, 5);
+                robot.arm(1);
+                robot.sleep(250);
+                robot.arm(0);
                 robot.counter++;
                 break;
             case 1:
-                rings = robot.distance();
+                robot.armServ(.5);
                 robot.counter++;
                 break;
             case 2:
-                robot.drive(2 * feet, 0, 5);
+                robot.arm(1);
+                robot.sleep(300);
+                robot.arm(0);
                 robot.counter++;
                 break;
+
             case 3:
-                robot.shoot();
+                robot.drive(2 * feet, 2 * feet, 5);
                 robot.counter++;
                 break;
             case 4:
-                robot.drive(1*feet, 0, 5);
+                rings = robot.distance();
                 robot.counter++;
                 break;
             case 5:
-                robot.shoot();
+                robot.drive(2 * feet, 0, 5);
                 robot.counter++;
                 break;
             case 6:
-                robot.drive(1*feet, 0, 5);
+                robot.shoot();
                 robot.counter++;
                 break;
             case 7:
+                robot.drive(1*feet, 0, 5);
+                robot.counter++;
+                break;
+            case 8:
+                robot.shoot();
+                robot.counter++;
+                break;
+            case 9:
+                robot.drive(1*feet, 0, 5);
+                robot.counter++;
+                break;
+            case 10:
                 if (rings == 4) {
                     robot.drive(-1 * feet, 4 * feet, 5);
                 } else if (rings == 1) {
@@ -80,8 +97,10 @@ int rings = 0;
                 }
                 robot.counter++;
                 break;
-            case 8:
-
+            case 11:
+                robot.armServ(1);
+                robot.counter++;
+                break;
 
 
                 telemetry.addData("Case:", robot.counter);
