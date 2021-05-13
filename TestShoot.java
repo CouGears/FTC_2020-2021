91b5a3a48a9f6d2ec7560ba015dcd01b19cdc8ad
Aaronthetechguy <aaron.goldgewert@gmail.com>
2021-05-13 16:55:50 -0400

    added testshoot

---
+++ TestShoot.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+
+public class TestShoot extends OpMode {
+    double rev = 383.6;
+    double inch = rev / (3.78 * 3.14);
+    double feet = inch * 12;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DigitalChannel DigChannel;
+    AutonMethods_Copy robot = new AutonMethods_Copy();
+    int diamond = 0;
+
+    public void init() {
+        robot.init(hardwareMap, telemetry, false);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+ @Override
+    public void loop() {
+        int rings = 0;
+        switch (robot.counter) {
+            case 0:
+                robot.scissorServUp();
+                robot.counter++;
+                break;
+            case 1:
+                robot.shoot(true);
+                robot.sleep(2000);
+                robot.topGoal();
+                robot.counter++;
+                break;
+            case 2:
+                robot.topGoal();
+                robot.counter++;
+                break;
+            case 3:
+                robot.topGoal();
+                robot.shoot(false);
+                robot.counter++;
+                break;
+            case 4:
+                robot.scissorServDown();
+                robot.counter++;
+                break;
+        }
+    }
+}
